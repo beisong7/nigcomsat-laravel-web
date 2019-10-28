05114d96cfba7d49f7ad5155dd9b8549b1ab0d45
Benjamin <beisongabc@gmail.com>
2019-10-28 15:51:35 +0100

    removed home from navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{ route('home') }}">
             <span class="strong">NIPTV</span>
             {{--<img src="{{ url('img/logo/nigcom-logo.png') }}" alt=""/>--}}
         </a>
@@ -10,11 +10,7 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('home') }}">Home
-                    <span class="sr-only">(current)</span>
-                    </a>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home')."#plan" }}">Plans</a>
                 </li>
