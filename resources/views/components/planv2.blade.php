fda488d08411be7e774000b9c2a2d8ffc43d71be
Benjamin <beisongabc@gmail.com>
2019-11-05 11:27:30 +0100

    plans updated

--- resources/views/components/planv2.blade.php
+++ resources/views/components/planv2.blade.php
@@ -3,69 +3,50 @@
         <div class="row">
             <div class="mb-5 fill-width"><h2 class="text-center text-white text-uppercase ">Select your preferred plan</h2></div>
         </div>
-        <div class="card-deck ">
+        <div class="row">
+            <div class="card-deck col">
 
-            <div class="card mb-5 mb-lg-0">
-                <div class="card-body">
-                    <h6 class="card-price text-center">NIPTV Free</h6>
+                <div class="card mb-5 mb-lg-0">
+                    <div class="card-body">
+                        <h6 class="card-price text-center">NIPTV Free</h6>
 
-                    <hr/>
-                    <ul class="fa-ul mb-100 ml-2 mr-2">
-                        <li>
-                            <p class="lead text-justify">
-                                Plug onto our largest streaming library. Enjoy full seasons of exclusive series, movies, kids shows, and more
-                            </p>
-                        </li>
+                        <hr/>
+                        <ul class="fa-ul mb-100 ml-2 mr-2">
+                            <li>
+                                <p class="lead text-justify">
+                                    Plug onto our largest streaming library. Enjoy full seasons of exclusive series, movies, kids shows, and more
+                                </p>
+                            </li>
 
 
-                    </ul>
-                    <a href="{{ route('subscribe','free') }}" class="btn btn-block btn-primary text-uppercase snap-bottom">Get First Month Free</a>
+                        </ul>
+                        <a href="{{ route('subscribe','free') }}" class="btn btn-block btn-primary text-uppercase snap-bottom">Get Two Weeks Free</a>
+                    </div>
                 </div>
-            </div>
 
-            <div class="card mb-5 mb-lg-0">
-                <div class="card-body">
-                    <h6 class="card-price text-center">NIPTV Plus</h6>
+                <div class="card mb-5 mb-lg-0">
+                    <div class="card-body">
+                        <h6 class="card-price text-center">NIPTV Pro</h6>
 
-                    <hr/>
-                    <ul class="fa-ul mb-100 ml-2 mr-2">
-                        <li>
-                            <p class="lead text-justify">
-                                Plug onto our largest streaming library with on demand TV Channels. Enjoy full seasons of exclusive series, movies, kids shows, and more.
-                            </p>
+                        <hr/>
+                        <ul class="fa-ul mb-100 ml-2 mr-2">
+                            <li>
+                                <p class="lead text-justify">
+                                    Plug onto our largest streaming library with Live and on Demand TV . Enjoy full seasons of exclusive series, movies, kids shows, and more.
+                                </p>
 
-                            <h5 class="text-muted text-center">Get First Month Free</h5>
-                            <h3 class=" text-center"><small>then pay</small> <br class="mb-3"> NGN500<span class="small">/Month</span></h3>
-                        </li>
-
-
-                    </ul>
-
-                    <a href="{{ route('subscribe','plus') }}" class="btn btn-block btn-primary text-uppercase snap-bottom">Select</a>
-                </div>
-            </div>
-
-            <div class="card mb-5 mb-lg-0">
-                <div class="card-body">
-                    <h6 class="card-price text-center">NIPTV Pro</h6>
-
-                    <hr/>
-                    <ul class="fa-ul mb-100 ml-2 mr-2">
-                        <li>
-                            <p class="lead text-justify">
-                                Plug onto our largest streaming library with Live and on Demand TV . Enjoy full seasons of exclusive series, movies, kids shows, and more.
-                            </p>
-
-                            <h5 class="text-muted text-center">Get First Month Free</h5>
-                            <h3 class=" text-center"><small>then pay</small> <br class="mb-3"> NGN1,000<span class="small">/Month</span></h3>
-                        </li>
+                                <h5 class="text-muted text-center">Get Two Weeks Free</h5>
+                                <h3 class=" text-center"><small>then pay</small> <br class="mb-3"> NGN1,000<span class="small">/Month</span></h3>
+                            </li>
 
 
-                    </ul>
+                        </ul>
 
-                    <a href="{{ route('subscribe','pro') }}" class="btn btn-block btn-primary text-uppercase snap-bottom">Select</a>
+                        <a href="{{ route('subscribe','pro') }}" class="btn btn-block btn-primary text-uppercase snap-bottom">Select</a>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </section>
