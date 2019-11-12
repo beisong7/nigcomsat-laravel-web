36285cb387006c005e7b4fa3666cc89d2a33b052
Benjamin <beisongabc@gmail.com>
2019-11-12 16:46:29 +0100

    new scripts added

--- resources/views/components/vids.blade.php
+++ resources/views/components/vids.blade.php
@@ -7,8 +7,18 @@
 
             <div class="col-md-6 col-sm-12 mb-5 text-center"><script type="text/javascript" src="https://cleeng.com/monetize/A344037558_NG?dimWidth=640&dimHeight=360"></script></div>
 
+            <div class="col-md-6 col-sm-12 mb-5 text-center">
+                <script type="text/javascript" src="https://cleeng.com/monetize/A664431925_NG?dimWidth=640&dimHeight=360"></script>
+            </div>
+            <div class="col-md-6 col-sm-12 mb-5 text-center">
+                <script type="text/javascript" src="https://cleeng.com/monetize/A407416606_NG?dimWidth=640&dimHeight=360"></script>
+            </div>
+
+
         </div>
     </div>
+
+    {{--<i class="fa fa-building"></i>--}}
 
 </div>
 
